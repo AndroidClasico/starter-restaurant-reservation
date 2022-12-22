50f3b44287a089ce5c0982015b0f3129f00a1764
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2022-12-22 09:31:35 -0700

    Update Routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -5,7 +5,7 @@ import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import ReservationCreate from "../reservation/ReservationCreate";
 import { today } from "../utils/date-time";
-import useQuery from "../utils/useQuery"
+import useQuery from "../utils/useQuery";
 
 /**
  * Defines all the routes for the application.
@@ -15,10 +15,10 @@ import useQuery from "../utils/useQuery"
  * @returns {JSX.Element}
  */
 function Routes() {
-
+  //testing
   const query = useQuery();
-  const date = query.get("date")
-  
+  const date = query.get("date");
+
   return (
     <Switch>
       <Route exact={true} path="/">
