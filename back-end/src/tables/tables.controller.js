73af5ece9b75a7a878f6602e591bc59a2698479f
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2022-12-27 06:16:40 -0700

    Update tables.controller.js

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -2,7 +2,7 @@ const service = require("./tables.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const reservationsService = require("../reservations/reservations.service");
 
-function bodyHasData(req, _res, next) {
+function bodyHasData(req, res, next) {
   const { data } = req.body;
   if (!data) {
     next({
@@ -13,7 +13,7 @@ function bodyHasData(req, _res, next) {
   next();
 }
 
-function hasCapacity(req, _res, next) {
+function hasCapacity(req, res, next) {
   const { capacity } = req.body.data;
   if (!capacity) {
     next({ status: 400, message: "capacity" });
@@ -22,7 +22,7 @@ function hasCapacity(req, _res, next) {
   }
 }
 
-function isValidCapacity(req, _res, next) {
+function isValidCapacity(req, res, next) {
   const { capacity } = req.body.data;
   if (capacity === 0 || !Number.isInteger(capacity)) {
     next({ status: 400, message: "capacity" });
@@ -42,15 +42,13 @@ function bodyHasReservationId(req, res, next) {
   next();
 }
 
-function isValidName(req, _res, next) {
+function isValidName(req, res, next) {
   const { table_name } = req.body.data;
   if (!table_name || !table_name.length || table_name.length === 1) {
     return next({ status: 400, message: "table_name" });
   }
   next();
 }
-
-
 
 async function tableExists(req, res, next) {
   const { table_id } = req.params;
@@ -66,67 +64,55 @@ async function tableExists(req, res, next) {
   }
 }
 
+function isTableLargeEnough(req, res, next) {
+  const { capacity } = res.locals.table;
+  const { people } = res.locals.reservation;
 
-function isTableLargeEnough(_req, res, next) {
-    const { capacity } = res.locals.table
-    const { people } = res.locals.reservation
-
-    if (Number(people) > Number(capacity)) {
-        return next({
-            status: 400, 
-            message: `The number of people in this party exceeds the capacity of the table`
-        })
-    }
-    next()
+  if (Number(people) > Number(capacity)) {
+    return next({
+      status: 400,
+      message: `The number of people in this party exceeds the capacity of the table`,
+    });
+  }
+  next();
 }
 
 // verifying that table is NOT occupied
-function isAvailable(_req, res, next) {
-    const { reservation_id } = res.locals.table
-    if(reservation_id) {
-        return next({
-            status: 400, 
-            message: `The table you selected is currently occupied by another party. Please select a different table.`
-        })
-    }
-    next()
+function isAvailable(req, res, next) {
+  const { reservation_id } = res.locals.table;
+  if (reservation_id) {
+    return next({
+      status: 400,
+      message: `The table you selected is currently occupied by another party. Please select a different table.`,
+    });
+  }
+  next();
 }
 
 // verifying that table is occupied
 
-async function reservationIdExists(_req, res, next) {
+async function reservationIdExists(req, res, next) {
   const reservation = await reservationsService.read(res.locals.reservation_id);
   if (!reservation) {
     return next({ status: 404, message: `${res.locals.reservation_id}` });
   } else {
     res.locals.reservation = reservation;
-  next();
+    next();
   }
 }
 
-
-function isReservationSeated(req, res, next) {
-  // reservation already seated
-  if (res.locals.reservation.status === "seated")
-    return next({
-      status: 400,
-      message: "reservation is already seated",
-    });
-  // reservation not seated
-  next();
-}
-
-async function create(req, res, _next) {
+async function create(req, res, next) {
   res.status(201).json({ data: await service.create(req.body.data) });
 }
 
-async function list(_req, res, _next) {
+async function list(req, res, next) {
   res.json({ data: await service.list() });
 }
 
-async function seat(_req, res, _next) {
+async function seat(req, res, next) {
   const data = await service.seat(
-    res.locals.table.table_id,
+    // res.locals.table.table_id,
+    res.locals.table,
     res.locals.reservation_id
   );
   res.json({ data });
@@ -145,7 +131,8 @@ module.exports = {
     bodyHasData,
     bodyHasReservationId,
     asyncErrorBoundary(reservationIdExists),
-    isReservationSeated,
+    isValidCapacity,
+    // isReservationSeated,
     asyncErrorBoundary(tableExists),
     isTableLargeEnough,
     isAvailable,
